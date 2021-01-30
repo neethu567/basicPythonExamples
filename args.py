3454bc34531b2b6882efd16ab151f446c3a7e660
neethu567 <46527855+neethu567@users.noreply.github.com>
2021-01-30 11:50:49 +0530

    Update args.py

--- args.py
+++ args.py
@@ -1,8 +1,9 @@
+"""multiple arguments/argumnet ,here we can give multiple actual argument and collect by using one *formal argument"""
 def mysum(a,b):
     print(a+b)
 
 mysum(10,22)
-"""same can be done by using below mwthod"""
+"""same can be done by using below method"""
 def mylist(*integers):
     s=0
     for i in integers:
